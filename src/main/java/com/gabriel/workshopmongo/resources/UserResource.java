d63b7bcf19e14ce9852ab32b66f90c3578023587
gabrielsantanaf <gabrielsantanaf09@gmail.com>
2026-04-18 23:59:48 -0300

    feat: Implementando DTO na rota de User.

--- src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.gabriel.workshopmongo.resources;
 
 import com.gabriel.workshopmongo.domain.User;
+import com.gabriel.workshopmongo.dto.UserDTO;
 import com.gabriel.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,9 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -19,9 +19,10 @@ public class UserResource {
     @Autowired
     private UserService service;
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 
-        return ResponseEntity.ok().body(list);
+        return ResponseEntity.ok().body(listDTO);
     }
 }
