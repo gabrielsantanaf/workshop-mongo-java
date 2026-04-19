f517b743117ae0fc39efb195be81a4d557f3a2e9
gabrielsantanaf <gabrielsantanaf09@gmail.com>
2026-04-19 00:41:09 -0300

    feat: obtendo user por id e criando captura de exceções em run time.

--- src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.gabriel.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +19,19 @@ public class UserResource {
 
     @Autowired
     private UserService service;
+
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
         List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User obj = service.findById(id);
+        UserDTO user = new UserDTO(obj);
+        return ResponseEntity.ok().body(user);
     }
 }
