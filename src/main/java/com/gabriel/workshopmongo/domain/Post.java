2889850169843c39c55029ac6fa67b1dc2348f71
gabrielsantanaf <gabrielsantanaf09@gmail.com>
2026-04-20 10:41:00 -0300

    feat: Adicionando comentarios aos posts.

--- src/main/java/com/gabriel/workshopmongo/domain/Post.java
+++ src/main/java/com/gabriel/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.gabriel.workshopmongo.domain;
 
 import com.gabriel.workshopmongo.dto.AuthorDTO;
+import com.gabriel.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -22,6 +25,7 @@ public class Post implements Serializable {
     private String body;
 
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post(){
     }
@@ -72,6 +76,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
--- src/main/java/com/gabriel/workshopmongo/resources/PostResource.java
+++ src/main/java/com/gabriel/workshopmongo/resources/PostResource.java
@@ -1,17 +1,10 @@
 package com.gabriel.workshopmongo.resources;
 
 import com.gabriel.workshopmongo.domain.Post;
-import com.gabriel.workshopmongo.domain.User;
-import com.gabriel.workshopmongo.dto.UserDTO;
 import com.gabriel.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/posts")
