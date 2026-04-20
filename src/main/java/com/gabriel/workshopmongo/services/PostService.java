be4b3d63a858013657c4714e8ffab33c1eb86d66
gabrielsantanaf <gabrielsantanaf09@gmail.com>
2026-04-20 00:23:38 -0300

    feat: Buscando post por ID

---
+++ src/main/java/com/gabriel/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.gabriel.workshopmongo.services;
+
+import com.gabriel.workshopmongo.domain.Post;
+import com.gabriel.workshopmongo.domain.User;
+import com.gabriel.workshopmongo.dto.UserDTO;
+import com.gabriel.workshopmongo.repository.PostRepository;
+import com.gabriel.workshopmongo.repository.UserRepository;
+import com.gabriel.workshopmongo.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+    public Post findById(String id){
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
